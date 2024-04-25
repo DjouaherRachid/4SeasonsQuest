41b8604c096c553398db568184165527165f32c3
Rachid <djouaherrachid@gmail.com>
2024-04-25 13:48:16 +0200

    Add bridge

--- Assets/Scripts/TreeZoneHorizontalGrowth.cs
+++ Assets/Scripts/TreeZoneHorizontalGrowth.cs
@@ -3,24 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TreeZoneHorizontalGrowth : MonoBehaviour
-{   public GameObject bucket;
-    public GameObject seed;
+{   
+    public GameObject bucket;
     public Animator animator; 
-    public Material fullMaterial;
-    public Material emptyMaterial;
+    public Renderer deadTree;
+    public Renderer cherryTree;
 
-    private bool isSeedInside = false; 
     private bool isBucketInside = false;
-    private static readonly int GrowTree = Animator.StringToHash("GrowTree");
-
+    private static readonly int RootGrowth = Animator.StringToHash("RootGrowth");
+        
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Seed"))
-        {
-            isSeedInside = true;
-            CheckAnimationTrigger();
-        }
-        else if (collision.gameObject == bucket)
+        if (collision.gameObject == bucket)
         {   
             // Vérifier si le seau est rempli d'eau
             if(bucket.GetComponent<BucketBehaviour>().isFull == true)
@@ -30,27 +24,14 @@ public class TreeZoneHorizontalGrowth : MonoBehaviour
             }
         }
     }
-
-    private void OnCollisionExit(Collision other)
-    {
-        if (other.gameObject.CompareTag("Seed"))
-        {
-            isSeedInside = false;
-            CheckAnimationTrigger();
-        }
-        /*
-        else if (other.gameObject.CompareTag("Bucket"))
-        {
-            isBucketInside = false;
-            CheckAnimationTrigger();
-        }*/
-    }
+    
     private void CheckAnimationTrigger()
     {
-        if (isSeedInside && isBucketInside)
+        if (isBucketInside)
         {
-            animator.SetTrigger(GrowRoot);
-            seed.SetActive(false);
+            animator.SetTrigger(RootGrowth);
+            deadTree.enabled = false;
+            cherryTree.enabled = true;
             bucket.SetActive(false);
         }
     }
